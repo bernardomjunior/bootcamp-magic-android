8a1e3ce0ceae937e2a038702cbe5cade63b919dc
Bernardo Junior <bernardodemsj@gmail.com>
2020-05-20 17:04:05 -0300

    Adds Catalog Activity and its dependencies

---
+++ app/src/main/java/com/bootcamp/concrete/magicdeck/ui/CatalogActivity.kt
@@ -0,0 +1,14 @@
+package com.bootcamp.concrete.magicdeck.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.bootcamp.concrete.magicdeck.R
+
+class CatalogActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_catalog)
+    }
+
+}
