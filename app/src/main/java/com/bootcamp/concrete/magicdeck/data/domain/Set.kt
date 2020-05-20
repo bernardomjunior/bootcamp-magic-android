c8be8bb9444f296206b7cdb1c509bbb8e42abb23
Bernardo Junior <bernardodemsj@gmail.com>
2020-05-20 14:07:16 -0300

    Creates Set data class and Api Interface with getSets

---
+++ app/src/main/java/com/bootcamp/concrete/magicdeck/data/domain/Set.kt
@@ -0,0 +1,6 @@
+package com.bootcamp.concrete.magicdeck.data.domain
+
+data class Set(
+    val code: String,
+    val name: String
+)
---
+++ app/src/main/java/com/bootcamp/concrete/magicdeck/data/external/ApiService.kt
@@ -0,0 +1,12 @@
+package com.bootcamp.concrete.magicdeck.data.external
+
+import com.bootcamp.concrete.magicdeck.data.domain.Set
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface ApiService {
+
+    @GET("sets/")
+    fun getSets(): Call<List<Set>>
+
+}
