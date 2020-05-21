d1fcf68a984bc0f52f624d36affe6d71633f0ab2
Bernardo Junior <bernardodemsj@gmail.com>
2020-05-21 14:51:51 -0300

    Creates Card List Adapter (WIP)

--- app/src/main/java/com/bootcamp/concrete/magicdeck/ui/activity/CatalogActivity.kt
+++ app/src/main/java/com/bootcamp/concrete/magicdeck/ui/activity/CatalogActivity.kt
@@ -1,14 +1,30 @@
 package com.bootcamp.concrete.magicdeck.ui.activity
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.bootcamp.concrete.magicdeck.R
+import com.bootcamp.concrete.magicdeck.data.domain.Card
+import com.bootcamp.concrete.magicdeck.ui.adapter.CardsListAdapter
+import kotlinx.android.synthetic.main.activity_catalog.*
 
 class CatalogActivity : AppCompatActivity() {
+
+    val cards = ArrayList<Card>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_catalog)
+        for(i in 1..100){
+            cards.add(Card("a$i", listOf("2$i"), "123$i", "123213$i", "123$i"))
+        }
+        setUpList()
+    }
+
+    fun setUpList(){
+        cards_catalog.adapter = CardsListAdapter(cards, this@CatalogActivity){
+            Toast.makeText(this@CatalogActivity, it.name, Toast.LENGTH_SHORT).show()
+        }
     }
 
 }
---
+++ app/src/main/java/com/bootcamp/concrete/magicdeck/ui/adapter/CardsListAdapter.kt
@@ -0,0 +1,36 @@
+package com.bootcamp.concrete.magicdeck.ui.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bootcamp.concrete.magicdeck.R
+import com.bootcamp.concrete.magicdeck.data.domain.Card
+import kotlinx.android.synthetic.main.card_item.view.image_card_item
+
+class CardsListAdapter(
+        private val cardList: List<Card>,
+        private val context: Context,
+        private val listener: (card: Card) -> Unit
+) : RecyclerView.Adapter<CardsListAdapter.ViewHolder>() {
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bind(card: Card, listener: (card: Card) -> Unit) {
+            itemView.image_card_item.setOnClickListener { listener(card) }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+                LayoutInflater.from(context).inflate(
+                        R.layout.card_item, parent, false)
+        )
+    }
+
+    override fun getItemCount() = cardList.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(cardList[position], listener)
+    }
+}
