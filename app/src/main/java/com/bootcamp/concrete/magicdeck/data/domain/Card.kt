92d3d0ead670df0e5a04cc3ff14314f7a8f13949
Bernardo Junior <bernardodemsj@gmail.com>
2020-05-23 20:57:57 -0300

    Adds card domain class

---
+++ app/src/main/java/com/bootcamp/concrete/magicdeck/data/domain/Card.kt
@@ -0,0 +1,13 @@
+package com.bootcamp.concrete.magicdeck.data.domain
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+class Card (
+    val name: String,
+    val types: List<String>,
+    val set: String,
+    val id: String,
+    val imageUrl: String?
+): Parcelable
