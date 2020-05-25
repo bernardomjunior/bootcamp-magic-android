2078482ede47f6cad2534175bfada6587aaecb10
Bernardo Junior <bernardodemsj@gmail.com>
2020-05-25 02:40:26 -0300

    Changes card listing adapter logic
    To suit the need of two different layouts logic

--- app/src/main/java/com/bootcamp/concrete/magicdeck/ui/adapter/CardsListAdapter.kt
+++ app/src/main/java/com/bootcamp/concrete/magicdeck/ui/adapter/CardsListAdapter.kt
@@ -7,17 +7,39 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bootcamp.concrete.magicdeck.R
 import com.bootcamp.concrete.magicdeck.data.domain.Card
+import com.bootcamp.concrete.magicdeck.data.domain.CardListHeader
+import com.bootcamp.concrete.magicdeck.data.domain.CardListItem
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.card_item.view.image_card_item
+import kotlinx.android.synthetic.main.list_header.view.txt_list_header
 
 class CardsListAdapter(
-        private val cardList: List<Card>,
-        private val context: Context,
-        private val listener: (card: Card) -> Unit
+    private val cardList: List<CardListItem>,
+    private val context: Context,
+    private val listener: (card: Card) -> Unit
 ) : RecyclerView.Adapter<CardsListAdapter.ViewHolder>() {
 
+    companion object {
+        const val CARD = 1
+    }
+
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        fun bind(card: Card, listener: (card: Card) -> Unit) {
+        fun bind(listItem: CardListItem, listener: (card: Card) -> Unit) {
+            when (listItem) {
+                is Card -> initCard(listItem, listener)
+                is CardListHeader -> initHeader(listItem)
+            }
+
+        }
+
+        private fun initHeader(header: CardListHeader) {
+            itemView.txt_list_header.text = header.text
+        }
+
+        private fun initCard(
+            card: Card,
+            listener: (card: Card) -> Unit
+        ) {
             Picasso.get()
                 .load(card.imageUrl)
                 .error(R.drawable.card_example)
@@ -26,11 +48,24 @@ class CardsListAdapter(
         }
     }
 
+    override fun getItemViewType(position: Int): Int {
+        return if (cardList[position] is CardListHeader) 3 else 1
+    }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        return ViewHolder(
+        if (viewType == CARD) {
+            return ViewHolder(
                 LayoutInflater.from(context).inflate(
-                        R.layout.card_item, parent, false)
+                    R.layout.card_item, parent, false
+                )
+            )
+        }
+        return ViewHolder(
+            LayoutInflater.from(context).inflate(
+                R.layout.list_header, parent, false
+            )
         )
+
     }
 
     override fun getItemCount() = cardList.size
