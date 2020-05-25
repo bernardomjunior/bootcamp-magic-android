3d1c3e7baaf759610e052d66acd3647f6c464545
Bernardo Junior <bernardodemsj@gmail.com>
2020-05-25 15:53:07 -0300

    Changes network proprieties to lazy
    To avoid unnecessary memory usage

--- app/src/main/java/com/bootcamp/concrete/magicdeck/data/external/ApiNetwork.kt
+++ app/src/main/java/com/bootcamp/concrete/magicdeck/data/external/ApiNetwork.kt
@@ -9,21 +9,24 @@ import java.util.concurrent.TimeUnit
 
 object ApiNetwork {
 
-    private val okhttp3 = OkHttpClient()
+    private val okhttp3 by lazy {  OkHttpClient()
         .newBuilder()
         .connectTimeout(60, TimeUnit.SECONDS)
         .readTimeout(60, TimeUnit.SECONDS)
         .build()
+    }
 
-    val retrofit: ApiService = Retrofit.Builder()
-        .baseUrl(BuildConfig.BASE_URL)
-        .client(okhttp3)
-        .addConverterFactory(
-            GsonConverterFactory.create(
-                GsonBuilder().create()
+    val retrofit: ApiService by lazy {
+        Retrofit.Builder()
+            .baseUrl(BuildConfig.BASE_URL)
+            .client(okhttp3)
+            .addConverterFactory(
+                GsonConverterFactory.create(
+                    GsonBuilder().create()
+                )
             )
-        )
-        .build()
-        .create(ApiService::class.java)
+            .build()
+            .create(ApiService::class.java)
+    }
 
 }
