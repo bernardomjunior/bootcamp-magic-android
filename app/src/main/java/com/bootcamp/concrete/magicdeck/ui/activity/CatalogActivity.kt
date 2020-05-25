f478b67f4b5fb98a90fc17469591312938410624
Bernardo Junior <bernardodemsj@gmail.com>
2020-05-25 02:42:20 -0300

    Catalog activity now deals with layoutmanager logic

--- app/src/main/java/com/bootcamp/concrete/magicdeck/ui/activity/CatalogActivity.kt
+++ app/src/main/java/com/bootcamp/concrete/magicdeck/ui/activity/CatalogActivity.kt
@@ -3,15 +3,18 @@ package com.bootcamp.concrete.magicdeck.ui.activity
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.GridLayoutManager
 import com.bootcamp.concrete.magicdeck.R
 import com.bootcamp.concrete.magicdeck.data.domain.Card
+import com.bootcamp.concrete.magicdeck.data.domain.CardListHeader
+import com.bootcamp.concrete.magicdeck.data.domain.CardListItem
 import com.bootcamp.concrete.magicdeck.ui.adapter.CardsListAdapter
 import com.bootcamp.concrete.magicdeck.ui.adapter.DividerItemDecoration
 import kotlinx.android.synthetic.main.activity_catalog.cards_catalog
 
 class CatalogActivity : AppCompatActivity() {
 
-    private val cards = ArrayList<Card>()
+    private val cards = ArrayList<CardListItem>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,6 +32,21 @@ class CatalogActivity : AppCompatActivity() {
         )
         cards_catalog.adapter = CardsListAdapter(cards, this@CatalogActivity) {
             startCardCarouselActivity(it)
+        }
+        val layoutManager =
+            GridLayoutManager(this@CatalogActivity, resources.getInteger(R.integer.list_span_size))
+        changeListItemSpanSize(layoutManager)
+        cards_catalog.layoutManager = layoutManager
+    }
+
+    private fun changeListItemSpanSize(layoutManager: GridLayoutManager) {
+        layoutManager.spanSizeLookup = object : GridLayoutManager.SpanSizeLookup() {
+            override fun getSpanSize(position: Int): Int {
+                cards_catalog.adapter?.let {
+                    return it.getItemViewType(position)
+                }
+                return 1
+            }
         }
     }
 
