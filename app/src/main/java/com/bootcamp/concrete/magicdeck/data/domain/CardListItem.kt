66d7f82a45821f9c167506525ea1b17ee0aeeb39
Bernardo Junior <bernardodemsj@gmail.com>
2020-06-02 16:26:15 -0300

    Annotate Card

--- app/src/main/java/com/bootcamp/concrete/magicdeck/data/domain/CardListItem.kt
+++ app/src/main/java/com/bootcamp/concrete/magicdeck/data/domain/CardListItem.kt
@@ -1,16 +1,25 @@
 package com.bootcamp.concrete.magicdeck.data.domain
 
 import android.os.Parcelable
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import kotlinx.android.parcel.Parcelize
 
 sealed class CardListItem
 
+@Entity
 @Parcelize
 class Card(
+    @ColumnInfo(name = "name")
     val name: String,
+    @ColumnInfo(name = "types")
     val types: List<String>,
+    @ColumnInfo(name = "set")
     val set: String,
+    @PrimaryKey
     val id: String,
+    @ColumnInfo(name = "image_url")
     val imageUrl: String?
 ) : Parcelable, CardListItem()
 
