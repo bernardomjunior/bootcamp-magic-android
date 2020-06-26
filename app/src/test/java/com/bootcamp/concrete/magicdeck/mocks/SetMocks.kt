86a6c1e0539672fa7867722c31989212fd0ef05c
Bernardo Junior <bernardodemsj@gmail.com>
2020-06-26 18:00:00 -0300

    Adds set mocks

---
+++ app/src/test/java/com/bootcamp/concrete/magicdeck/mocks/SetMocks.kt
@@ -0,0 +1,17 @@
+package com.bootcamp.concrete.magicdeck.mocks
+
+import com.bootcamp.concrete.magicdeck.data.domain.Set
+import com.bootcamp.concrete.magicdeck.data.external.ResultWrapper
+import com.bootcamp.concrete.magicdeck.extension.getJson
+import java.lang.IllegalArgumentException
+
+const val setFile = "sets.json"
+
+fun successMockMapSet(classLoader: ClassLoader?): Map<String, List<Set>> {
+    return classLoader?.getJson(setFile) ?: throw IllegalArgumentException("Null ClassLoader passed as parameter")
+}
+
+fun successMockListSet(classLoader: ClassLoader?): ResultWrapper<List<Set>> {
+    val setMap = successMockMapSet(classLoader)
+    return ResultWrapper.Success(setMap.values.toList()[0])
+}
